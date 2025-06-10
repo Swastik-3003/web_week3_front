acce73aab2edc2f1622cdf03cfc69d88a22a349f
Swastik-3003 <swastik30032005@gmail.com>
2025-06-10 14:39:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LandingPage from './components/LandingPage';
+import { useState } from 'react';
+
+
+function App() {
+
+  return (
+    
+    <LandingPage />
+  ) 
+}
+
+export default App;
